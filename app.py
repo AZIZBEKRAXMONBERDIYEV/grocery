f94662cd3ee698f9cb9f85d06afba19256b88b22
AZIZBEKRAXMONBERDIYEV <jxechkim079@gmail.com>
2023-07-10 21:40:12 +0500

    navbatchi

--- app.py
+++ app.py
@@ -1,46 +1,31 @@
-from flask import Flask, render_template, request
-from db import get_all, get_by_id, insert, update, delete, get_by_price
+from flask import Flask, render_template, request ,jsonify
+from tinydb import TinyDB, Query
+
+
+db = TinyDB('db.json', indent=4)
+grocery = db.table('grocery')
+
+
 
 app = Flask(__name__)
+
+    
+product = db.table('grocery')
 
 
 @app.route('/')
 def index():
-    groceries = get_all()
+    groceries = grocery.all()
 
     return render_template('index.html', groceries=groceries)
 
 
-@app.route('/<id>')
+@app.route('/id/<id>')
 def id(id):
-    grocery = get_by_id(id)
+    grocery =product.get(doc_id=id)
 
     return render_template('id.html',  grocery= grocery)
 
-@app.route('/details/<int:id>')
-def details(id: int):
-    grocery = get_by_id(id)
-
-    return render_template('details.html', grocery=grocery)
-
-@app.route('/price')
-def in_range():
-    min = float(request.args.get('min'))
-    max = float(request.args.get('max'))
-
-    groceries = get_by_price(min, max)
-
-    return render_template('index.html', groceries=groceries)
-
-# delete route
-@app.route('/delete/<int:id>')
-def delete_grocery(id: int):
-    pass
-
-# insert route
-@app.route('/insert', methods=['POST'])
-def insert_grocery():
-    pass
 
 
 
